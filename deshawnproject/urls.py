8719bd9c8488491588cd75f526fea5e367a6eb47
Ethan Brown <eb934053@gmail.com>
2025-03-17 13:41:40 -0500

    Registered new route to /appointments

--- deshawnproject/urls.py
+++ deshawnproject/urls.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from deshawnapi.views import WalkerView, CityView, DogView
+from deshawnapi.views import WalkerView, CityView, DogView, AppointmentsView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'walkers', WalkerView, 'walk')
 router.register(r'cities', CityView, 'city')
 router.register(r'dogs', DogView, 'dog')
+router.register(r'appointments', AppointmentsView, 'appointment')
+# The first argument is the routing path
+# The second argument is the view that will handle client requests
+# The third argument is needed for the registration process of the URL
 
 urlpatterns = [
     path('', include(router.urls)),
